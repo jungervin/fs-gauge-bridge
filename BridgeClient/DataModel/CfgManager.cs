afe08312b44c41d1b234ec661528ab5c9e23c00e
Dave Amenta <dave@daveamenta.com>
2021-02-06 18:36:10 -0800

    Ignore bad gauges, update Steam path template

--- BridgeClient/DataModel/CfgManager.cs
+++ BridgeClient/DataModel/CfgManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -18,48 +19,76 @@ namespace BridgeClient.DataModel
             {
                 var airplaneDirectoryName = Path.GetFileName(Path.GetDirectoryName(cfg));
 
-                // Load aircraft.cfg
-                var aircraftCfg = new CfgFile(vfs.Resolve(cfg));
-                aircraftCfg.ReadMultipleSections("FLTSIM.", (section) =>
+                try
                 {
-                    if (section.ContainsKey("title"))
-                    Trace.WriteLine($"Title: {section["title"]}");
-                    titleToAircraftDirectoryName[section["title"].Replace("\"", "")] = airplaneDirectoryName;
-                });
-
-                // Load panel.cfg
-                var relativePanelCfg = Path.Combine(@"simobjects\airplanes", airplaneDirectoryName, @"panel\panel.cfg");
-                var resolvedPanelCfg = vfs.Resolve(relativePanelCfg);
-                if (resolvedPanelCfg != null)
-                {
-                    var panelCfg = new CfgFile(resolvedPanelCfg);
-                    var gauges = new List<string>();
-                    panelCfg.ReadMultipleSections("VCOCKPIT0", (section) =>
+                    // Load aircraft.cfg
+                    var aircraftCfg = new CfgFile(vfs.Resolve(cfg));
+                    aircraftCfg.ReadMultipleSections("FLTSIM.", (section) =>
                     {
-                        var key = section[section.Keys.FirstOrDefault(j => j.StartsWith("htmlgauge"))];
-                        gauges.Add(key);
-                        Trace.WriteLine($"htmlgauge: {key}");
-                    }, 1);
-                    aircraftDirectoryNameToGaugeList[airplaneDirectoryName] = gauges;
+                        if (section.ContainsKey("title"))
+                            Trace.WriteLine($"Title: {section["title"]}");
+                        titleToAircraftDirectoryName[section["title"].Replace("\"", "")] = airplaneDirectoryName;
+                    });
                 }
-                else
+                catch (Exception ex)
                 {
-                    Trace.WriteLine("No panel.cfg found");
+                    Trace.WriteLine("Failed to load panel.cfg " + ex);
                 }
-                // Load cockpit.cfg
-                var relativeCockpitCfg = Path.Combine(@"simobjects\airplanes", airplaneDirectoryName, @"cockpit.cfg");
-                var resolvedCockpitCfg = vfs.Resolve(relativeCockpitCfg);
-                if (resolvedCockpitCfg != null)
+                try
                 {
-                    var cockpitCfg = new CfgFile(resolvedCockpitCfg, true);
+                    // Load panel.cfg
+                    var relativePanelCfg = Path.Combine(@"simobjects\airplanes", airplaneDirectoryName, @"panel\panel.cfg");
+                    var resolvedPanelCfg = vfs.Resolve(relativePanelCfg);
+                    if (resolvedPanelCfg != null)
+                    {
+                        var panelCfg = new CfgFile(resolvedPanelCfg);
+                        var gauges = new List<string>();
+                        panelCfg.ReadMultipleSections("VCOCKPIT0", (section) =>
+                        {
+                            var gaugeKey = section.Keys.FirstOrDefault(j => j.StartsWith("htmlgauge"));
+                            if (string.IsNullOrWhiteSpace(gaugeKey))
+                            {
+                                Trace.WriteLine("Gauge key not found!");
+                            }
+                            else
+                            {
+                                var key = section[gaugeKey];
+                                gauges.Add(key);
+                                Trace.WriteLine($"htmlgauge: {key}");
+                            }
 
-                    aircraftDirectoryNameToCockpitCfg[airplaneDirectoryName] = cockpitCfg;
+                        }, 1);
+                        aircraftDirectoryNameToGaugeList[airplaneDirectoryName] = gauges;
+                    }
+                    else
+                    {
+                        Trace.WriteLine("No panel.cfg found");
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    Trace.WriteLine("No cockpit.cfg found");
+                    Trace.WriteLine("Failed to load panel.cfg " + ex);
                 }
+                try
+                {
+                    // Load cockpit.cfg
+                    var relativeCockpitCfg = Path.Combine(@"simobjects\airplanes", airplaneDirectoryName, @"cockpit.cfg");
+                    var resolvedCockpitCfg = vfs.Resolve(relativeCockpitCfg);
+                    if (resolvedCockpitCfg != null)
+                    {
+                        var cockpitCfg = new CfgFile(resolvedCockpitCfg, true);
 
+                        aircraftDirectoryNameToCockpitCfg[airplaneDirectoryName] = cockpitCfg;
+                    }
+                    else
+                    {
+                        Trace.WriteLine("No cockpit.cfg found");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine("Failed to load panel.cfg " + ex);
+                }
             }
         }
     }
